b335a3023c6ca88a072f495b3527275f96f1be46
carlos.medeiros <carlos.medeiros@el.com.br>
2022-01-15 15:08:08 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,29 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie"
 
 import { BASE_URL } from "utils/requests"
 
 function Listing() {
 
-    // forma incorreta - teste
+    const [pageNumber, setPageNumber] = useState(0);
 
-    axios.get(`${BASE_URL}/movies`)
-    .then(response => {
-        console.log(response.data)
-    })
+    useEffect(()=>{
+
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            const data = response.data as MoviePage
+            setPageNumber(data.number)
+            console.log(data)
+        })
+
+    }, []);    
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
